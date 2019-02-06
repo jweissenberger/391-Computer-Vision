3a3000afeb18c2fa4a6b2389175f5224ca3c3ce5
Jack Weissenberger <johnaweissenberger@gmail.com>
2019-02-06 14:13:25 -0500

    finish part 1

--- Lab_1/spatial_filtering.py
+++ Lab_1/spatial_filtering.py
@@ -8,7 +8,6 @@ import cv2
 
 
 if __name__ == '__main__':
-
     """
     img = cv2.imread('puppy.jpg')
 
@@ -28,8 +27,34 @@ if __name__ == '__main__':
         for j in range(k):
             print('1/%d, ' % k, end='')
         print("")
-        
-    """
+
+    # 3.2.1
 
     # gaussian filters on puppy noise data
     img = cv2.imread('noise_puppy.JPG')
+    cv2.imshow('myImage', img)
+
+    # gaussian blur
+    key = cv2.waitKey(0)
+    gauss = cv2.GaussianBlur(img, (27, 27), 0)
+    cv2.imwrite('gauss_puppy.JPG', gauss)
+    # the larger the kernel of the blur the mor of the noise seems to be reduced but
+    # the image also becomes more blurred and we seem to lose more information in the photo
+    cv2.imshow("gaus", gauss)
+    key = cv2.waitKey(0)
+
+    # this is the median blur
+    median = cv2.medianBlur(img, 27)
+    cv2.imwrite('median_puppy.JPG', median)
+    cv2.imshow("med", median)
+    key = cv2.waitKey(0)
+    """
+    # 3.2.2
+    img = cv2.imread('noise_puppy.jpg')
+    edges = cv2.Canny(img,100,200)
+    cv2.imshow("puppy edges", edges)
+    key = cv2.waitKey(0)
+    img = cv2.imread('window-05-01.jpg')
+    edges2 = cv2.Canny(img,100,200)
+    cv2.imshow("window edges", edges2)
+    key = cv2.waitKey(0)
